29d5b0c7e5990846e30aa4513b8e334fe9d9a083
Shane D Wilson <me@shanedwilson.com>
2018-10-01 20:25:25 -0500

    characters populated

--- javascripts/components/characters.js
+++ javascripts/components/characters.js
@@ -1,1 +1,31 @@
-console.log('characters.js');
+const characters = [
+    {
+        id: 'character1',
+        name: 'Daenerys Targaryen',
+        house: 'Targaryen',
+        imageUrl: 'https://vignette.wikia.nocookie.net/gameofthrones/images/5/5f/Daenerys_Dragonpit.jpg/revision/latest?cb=20171015095128'
+    },
+
+    {
+        id: 'character2',
+        name: 'Jon Snow',
+        house: "Night's Watch",
+        imageUrl: 'https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553'
+    },
+
+    {
+        id: 'character3',
+        name: 'Tyrion Lannister',
+        house: 'Lannister',
+        imageUrl: 'https://vignette.wikia.nocookie.net/gameofthrones/images/5/58/Tyrion_main_s7_e6.jpg/revision/latest?cb=20170818050344',
+    },
+
+    {
+        id: 'character4',
+        name: 'Eddard Stark',
+        house: 'Stark',
+        imageUrl: 'https://vignette.wikia.nocookie.net/gameofthrones/images/3/37/Eddard_Stark_infobox_new.jpg/revision/latest?cb=20160730050722'
+    }
+];
+
+console.log(characters)
